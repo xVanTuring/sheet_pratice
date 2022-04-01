2fd0863a141d02d223a51bf51e6b35f88bfa7d1c
xVanTuring <xVanTuring@foxmail.com>
2022-04-01 23:49:12 +0800

    音符范围

--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,77 @@
+const noteOrder: NoteName[] = ["c", "d", "e", "f", "g", "a", "b"];
+type NoteName = "c" | "d" | "e" | "f" | "g" | "a" | "b";
+function nextNoteName(note: NoteName): NoteName {
+  if (note === "g") {
+    return "a";
+  }
+  return String.fromCharCode(note.charCodeAt(0) + 1) as NoteName;
+}
+/**
+ * less than 0 if a<b
+ * larger than 1 if a>b
+ * 0 if equal
+ * @param a
+ * @param b
+ */
+function compareNote(
+  a: { noteName: NoteName; group: number },
+  b: { noteName: NoteName; group: number }
+) {
+  if (a.noteName === b.noteName && a.group === b.group) {
+    return 0;
+  }
+  if (a.group > b.group) {
+    return 1;
+  }
+  if (a.group < b.group) {
+    return -1;
+  }
+  if (noteOrder.indexOf(a.noteName) > noteOrder.indexOf(b.noteName)) {
+    return 1;
+  }
+  return -1;
+}
+function nextNote(a: { noteName: NoteName; group: number }) {
+  return {
+    noteName: nextNoteName(a.noteName),
+    group: a.noteName === "b" ? a.group + 1 : a.group,
+  };
+}
+/**
+ * both includes
+ * @param from
+ * @param to
+ */
+function noteRange(from: string, to: string) {
+  let range: string[] = [];
+  let initial = {
+    noteName: from.split("/")[0] as NoteName,
+    group: parseInt(from.split("/")[1]),
+  };
+  const last = {
+    noteName: to.split("/")[0] as NoteName,
+    group: parseInt(to.split("/")[1]),
+  };
+  while (compareNote(initial, last) <= 0) {
+    range.push(`${initial.noteName}/${initial.group}`);
+    initial = nextNote(initial);
+  }
+  return range;
+}
+const trebleNoteRange = noteRange("f/3", "e/6");
 function randomOneTrebleNote(): string {
-  const noteList = ["a", "b", "c", "d", "e", "f", "g"];
-  const pos = [3, 4, 5];
-  const randomElement = noteList[Math.floor(Math.random() * noteList.length)];
-  const randomPos = pos[Math.floor(Math.random() * pos.length)];
-  if (randomPos === 3 && ["c", "d", "e"].includes(randomElement)) {
-    return randomOneTrebleNote();
-  }
-  return `${randomElement}/${randomPos}`;
+  const randomElement =
+    trebleNoteRange[Math.floor(Math.random() * trebleNoteRange.length)];
+  return randomElement;
 }
-
+const bassNoteRange = noteRange("f/1", "g/4");
 function randomOneBassNote(): string {
-  const noteList = ["a", "b", "c", "d", "e", "f", "g"];
-  const pos = [2, 3, 4];
-  const randomElement = noteList[Math.floor(Math.random() * noteList.length)];
-  const randomPos = pos[Math.floor(Math.random() * pos.length)];
-  if (randomPos === 4 && ["g", "a", " b"].includes(randomElement)) {
-    return randomOneBassNote();
-  }
-  return `${randomElement}/${randomPos}`;
+  const randomElement =
+    bassNoteRange[Math.floor(Math.random() * bassNoteRange.length)];
+  return randomElement;
 }
 
+console.log(bassNoteRange, trebleNoteRange);
 export function randomOneNote(clef: "bass" | "treble") {
   if (clef === "bass") {
     return randomOneBassNote();
